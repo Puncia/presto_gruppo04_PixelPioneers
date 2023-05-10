3f36ff7e63cb5c78b00eebec958f48d72a9b7ce4
puncia <puncia@live.com>
2023-05-10 11:01:17 +0200

    Inserimento bottone logout

--- resources/views/components/nav.blade.php
+++ resources/views/components/nav.blade.php
@@ -1,49 +1,47 @@
 <nav class="nav_custom navbar navbar-expand-lg bg-body-tertiary bg_blu">
-    <div class="container-fluid">
-      <a class="navbar-brand" href="#">Presto.it</a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse " id="navbarNav">
-        <ul class="navbar-nav">
-          <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="{{route('welcome')}}">Home</a>
-          </li>
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Presto.it</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse " id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="{{route('welcome')}}">Home</a>
+        </li>
 
 
 
-          @guest
-          <li class="nav-item">
-            <a class="nav-link" href="{{route('login')}}">Accedi</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="{{route('register')}}">Registrati</a>
-          </li>
+        @guest
+        <li class="nav-item">
+          <a class="nav-link" href="{{route('login')}}">Accedi</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="{{route('register')}}">Registrati</a>
+        </li>
 
-          @else
+        @else
 
-          <li class="nav-item">
-            <a class="nav-link" href="">il tuo profilo</a>
-          </li>
+        <li class="nav-item">
+          <a class="nav-link" href="">il tuo profilo</a>
+        </li>
 
 
-          <li class="nav-item">
-            <a class="nav-link" href="">Tutti gli annunci</a>
-          </li>
+        <li class="nav-item">
+          <a class="nav-link" href="">Tutti gli annunci</a>
+        </li>
 
-          <li class="nav-item">
-            <a class="nav-link" href="">Inserisci album</a>
-          </li>
+        <li class="nav-item">
+          <a class="nav-link" href="">Inserisci album</a>
+        </li>
 
-          <div class="nav-link" id="nav-bar-logoutbutton">
-            <form method="POST" action="">
-                @csrf
-                <button class="btn btn-light" type="submit">Logout</button>
-            </form>
-        </div>
+        <a class="nav-link" href="{{ route('logout')}}" onclick="event.preventDefault(); 
+                    document.getElementById('logout-form').submit();">Logout</a>
+        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+          @csrf
           @endguest
 
-        </ul>
-      </div>
+      </ul>
     </div>
-  </nav>
+  </div>
+</nav>
