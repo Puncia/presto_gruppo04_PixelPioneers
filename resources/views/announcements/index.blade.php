dd5518045667e726c79afcf21c3b30fdcf28834c
Cristina Enas <enascritina33@gmail.com>
2023-05-15 00:42:32 +0200

    sistemato flex model pagina tutti gli annunci + eseguito riassunto user 10

--- resources/views/announcements/index.blade.php
+++ resources/views/announcements/index.blade.php
@@ -1,25 +1,27 @@
 <x-layout>
-    <div class="class col-8 ">
-        <div class="row">
-            @foreach ($announcements as $announcement)
-                <div class="class col-12 col-md-4 my-4">
-                    <div class="card shadow" style="width: 18rem;">
-                        <img src="https://picsum.photos/200" class="card-img-top p-3 rounded">
-                        <div class="card-body">
-                            <h5 class="card-title">{{ $announcement->title }}</h5>
-                            <p class="card-text">{{ $announcement->body }}</p>
-                            <p class="card-text">{{ $announcement->price }}</p>
-                            <a href="{{ route('announcements.show', compact('announcement')) }}"
-                                class="btn btn-primary shadow">Visualizza</a>
-                            <p class="card-footer my-2">Pubblicato il: {{ $announcement->created_at->format('d/m/Y') }}
-                                Autore: {{ $announcement->user->name ?? '' }}</p>
-                            <a href="{{ route('categoryShow', ['category' => $announcement->category]) }}">Categoria:
-                                {{ $announcement->category->name }}</a>
+    <div class="container-fluid">
+        <div class="class col-12 d-flex">
+            <div class="row d-flex justify-content-evenly">
+                @foreach ($announcements as $announcement)
+                    <div class="class col-12 col-md-4 my-5 d-flex justify-content-center">
+                        <div class="card shadow" style="width: 18rem;">
+                            <img src="https://picsum.photos/200" class="card-img-top p-3 rounded">
+                            <div class="card-body">
+                                <h5 class="card-title">{{ $announcement->title }}</h5>
+                                <p class="card-text">{{ $announcement->body }}</p>
+                                <p class="card-text">{{ $announcement->price }}</p>
+                                <a href="{{ route('announcements.show', compact('announcement')) }}"
+                                    class="btn btn-primary shadow">Visualizza</a>
+                                <p class="card-footer my-2">Pubblicato il: {{ $announcement->created_at->format('d/m/Y') }}
+                                    Autore: {{ $announcement->user->name ?? '' }}</p>
+                                <a href="{{ route('categoryShow', ['category' => $announcement->category]) }}">Categoria:
+                                    {{ $announcement->category->name }}</a>
+                            </div>
                         </div>
                     </div>
-                </div>
-            @endforeach
-            {{ $announcements->links() }}
+                @endforeach
+                {{ $announcements->links() }}
+            </div>
         </div>
     </div>
 </x-layout>
