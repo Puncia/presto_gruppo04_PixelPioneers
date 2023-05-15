5f19884ecc49fb15ed4f5b2242d97f8c78ea82b6
puncia <puncia@live.com>
2023-05-15 18:31:09 +0200

    Sistemata navbar e tasto ricerca

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -17,30 +17,6 @@
             <p class="border-bottom p-3 f2 tx-sec text_shadow">Cogli l'opportunità che hai sempre cercato!</p>
             <h1 class="fs-1 f1">Benvenuti nel nostro e-commerce</h1>
         </div>
-
-        {{--    <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
-            <div class="carousel-inner">
-                <div class="carousel-item active">
-                    <img src="https://www.apple.com/newsroom/images/product/iphone/standard/Apple-iPhone-14-Pro-iPhone-14-Pro-Max-hero-220907_Full-Bleed-Image.jpg.large.jpg" class="img-custom img-fluid">
-                </div>
-                <div class="carousel-item">
-                    <img src="https://helpguide.sony.net/speaker/srs-xb12/v1/it/contents/image/SRS_XB12_Black_front_up-Large.png" class="img-custom img-fluid">
-                </div>
-                <div class="carousel-item">
-                    <img src="https://cdn.pixabay.com/photo/2014/07/04/13/41/auto-383897_1280.jpg" class="img-custom img-fluid">
-                </div>
-            </div>
-            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying"
-                data-bs-slide="prev">
-                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span class="visually-hidden">Previous</span>
-            </button>
-            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying"
-                data-bs-slide="next">
-                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                <span class="visually-hidden">Next</span>
-            </button>
-        </div> --}}
         <div class="swiper mySwiper">
             <div class="swiper-wrapper">
                 <div class="swiper-slide">
@@ -71,7 +47,7 @@
         </div>
         {{-- ANNUNCI --}}
 
-        <h1 class="fs-1 f1 text-center m-5 ">Ultimi inseriti:</h1>
+        <h1 class="fs-1 f1 text-center m-5 border-top py-3">Ultimi inseriti:</h1>
         <div class="container-fluid">
             <div class="row ">
                 @foreach ($announcements as $announcement)
@@ -87,7 +63,7 @@
                                     class="my-2 border-top pt-2 border-dark card-link shadow btn_custom btn text-light">Categoria:
                                     {{ $announcement->category->name }}</a>
                                 <p class="card-footer">Pubblicato il:
-                                    {{ $announcement->created_at->format('d/m/Y H:m') }}
+                                    {{ $announcement->created_at->format('d/m/Y') }}
                                 </p>
                             </div>
                         </div>
