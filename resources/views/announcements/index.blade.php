73928e02f3a271e68a30f833d786f4d94be29afa
puncia <puncia@live.com>
2023-05-15 18:50:27 +0200

    Aggiunto controllo degli annunci a null nella lista degli annunci

--- resources/views/announcements/index.blade.php
+++ resources/views/announcements/index.blade.php
@@ -3,23 +3,25 @@
         <div class="col-12 d-flex">
             <div class="row d-flex justify-content-evenly">
                 @forelse ($announcements as $announcement)
-                    <div class="col-12 col-md-4 my-5 d-flex justify-content-center">
-                        <div class="card shadow">
-                            <img src="https://picsum.photos/600" class="card-img-top p-3 rounded">
-                            <div class="card-body">
-                                <h5 class="card-title">{{ $announcement->title }}</h5>
-                                <p class="card-text">{{ $announcement->body }}</p>
-                                <p class="card-text">{{ $announcement->price }}</p>
-                                <a href="{{ route('announcements.show', compact('announcement')) }}"
-                                    class="btn_custom btn text-light">Visualizza</a>
-                                <p class="card-footer my-2">Pubblicato il:
-                                    {{ $announcement->created_at->format('d/m/Y') }}
-                                    Autore: {{ $announcement->user->name ?? '' }}</p>
-                                <a href="{{ route('categoryShow', ['category' => $announcement->category]) }}">Categoria:
-                                    {{ $announcement->category->name }}</a>
+                    @if ($announcement->is_accepted != null)
+                        <div class="col-12 col-md-4 my-5 d-flex justify-content-center">
+                            <div class="card shadow">
+                                <img src="https://picsum.photos/600" class="card-img-top p-3 rounded">
+                                <div class="card-body">
+                                    <h5 class="card-title">{{ $announcement->title }}</h5>
+                                    <p class="card-text">{{ $announcement->body }}</p>
+                                    <p class="card-text">{{ $announcement->price }}</p>
+                                    <a href="{{ route('announcements.show', compact('announcement')) }}"
+                                        class="btn_custom btn text-light">Visualizza</a>
+                                    <p class="card-footer my-2">Pubblicato il:
+                                        {{ $announcement->created_at->format('d/m/Y') }}
+                                        Autore: {{ $announcement->user->name ?? '' }}</p>
+                                    <a href="{{ route('categoryShow', ['category' => $announcement->category]) }}">Categoria:
+                                        {{ $announcement->category->name }}</a>
+                                </div>
                             </div>
                         </div>
-                    </div>
+                    @endif
                 @empty
                     <div class="col-12">
                         <div class="alert alert-warning py-3 shadow">
