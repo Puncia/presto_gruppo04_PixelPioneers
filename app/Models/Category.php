a6a8cf82421ca8606d9d8554681622941593cb3b
puncia <puncia@live.com>
2023-05-20 15:09:43 +0200

    Aggiornate categorie

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -14,4 +14,37 @@ class Category extends Model
     {
         return $this->hasMany(Announcement::class);
     }
+
+    public function getCategoryLocale()
+    {
+        switch ($this->id) {
+            case 1:
+                return __('ui.M');
+                break;
+            case 2:
+                return __('ui.I');
+                break;
+            case 3:
+                return __('ui.E');
+                break;
+            case 4:
+                return __('ui.L');
+                break;
+            case 5:
+                return __('ui.G');
+                break;
+            case 6:
+                return __('ui.S');
+                break;
+            case 7:
+                return __('ui.Im');
+                break;
+            case 8:
+                return __('ui.T');
+                break;
+            case 9:
+                return __('ui.A');
+                break;
+        }
+    }
 }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -70,14 +70,14 @@
                                     <p>{{ $announcement->body }}</p>
                                     <p>{{ $announcement->price }}€</p>
                                     <a href="{{ route('announcements.show', compact('announcement')) }}"
-                                        class="justify-content btncard text-light">{{__('ui.Show')}}</a>
+                                        class="justify-content btncard text-light">{{ __('ui.Show') }}</a>
                                     <a href=""
-                                        class="my-2 border-top pt-2 border-dark shadow btncard text-light">{{__('ui.Categorie')}}
-                                        {{ $announcement->category->name }}</a> 
-                                        {{-- chiedere come cambiare le categorie!! --}}
-                                    <p>{{__('ui.Public')}}
+                                        class="my-2 border-top pt-2 border-dark shadow btncard text-light">{{ __('ui.Categoria') }}
+                                        {{ $announcement->category->getCategoryLocale() }}</a>
+                                    {{-- chiedere come cambiare le categorie!! --}}
+                                    <p>{{ __('ui.Public') }}
                                         {{ $announcement->created_at->format('d/m/Y') }}
-                                        {{__('ui.Autore')}} {{ $announcement->user->name ?? '' }}
+                                        {{ __('ui.Autore') }} {{ $announcement->user->name ?? '' }}
                                     </p>
                                 </div>
                                 <div class="left-side"></div>
