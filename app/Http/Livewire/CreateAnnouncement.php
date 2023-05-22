04b06629f5be5c1494e0f50bfaff59c434a4726e
puncia <puncia@live.com>
2023-05-22 09:17:23 +0200

    Aumentata dimensione max file

--- app/Http/Livewire/CreateAnnouncement.php
+++ app/Http/Livewire/CreateAnnouncement.php
@@ -34,9 +34,9 @@ class CreateAnnouncement extends Component
         'title' => 'required|min:4',
         'body' => 'required |min:8',
         'category' => 'required',
-        'images.*' => 'image|max:1024',
+        'images.*' => 'image|max:4096',
         'price' => 'required|numeric',
-        'temporary_images.*' => 'image|max:1024',
+        'temporary_images.*' => 'image|max:4096',
     ];
 
     protected $messages = [
@@ -83,11 +83,10 @@ class CreateAnnouncement extends Component
                 $newImage = $this->announcement->images()->create(['path' => $image->store($newFileName, 'public')]);
 
                 RemoveFaces::withChain([
-                (new ResizeImage($newImage->path, 400, 300)),
-                (new GoogleVisionSafeSearch($newImage->id)),
-                (new GoogleVisionLabelImage($newImage->id)),
+                    (new ResizeImage($newImage->path, 400, 300)),
+                    (new GoogleVisionSafeSearch($newImage->id)),
+                    (new GoogleVisionLabelImage($newImage->id)),
                 ])->dispatch($newImage->id);
-                
             }
 
             File::deleteDirectory(storage_path('/app/livewire-tmp'));
